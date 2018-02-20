4aa2eb21d4296a057704b6923d232d510eff6a36
Jonathan Keljo <jkeljo@users.noreply.github.com>
2018-02-19 17:37:24 -0800

    Add packaging config

---
+++ setup.py
@@ -0,0 +1,33 @@
+from codecs import open
+from os import path
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="sisyphus-control",
+    version="0.1",
+    description="Control your Sisyphus kinetic art tables "
+                "(sisyphus-industries.com)",
+    long_description=long_description,
+    url="https://github.com/jkeljo/sisyphus-control",
+    author="Jonathan Keljo",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Home Automation",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords="sisyphus",
+    packages=["sisyphus/control"],
+    install_requires=[
+        "aiohttp",
+        "netifaces",
+    ],
+    python_requires="~=3.6",
+)
