23736bcef2119fa427f2d8811280d90168dd32ee
Jonathan Keljo <jkeljo@users.noreply.github.com>
2018-03-07 20:15:51 -0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name="sisyphus-control",
-    version="0.1",
+    version="0.1.1",
     description="Control your Sisyphus kinetic art tables "
                 "(sisyphus-industries.com)",
     long_description=long_description,
