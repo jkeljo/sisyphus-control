f7b687a9b7838cb2d8b06fb17c55d03f34a19199
Jonathan Keljo <jkeljo@users.noreply.github.com>
2021-10-20 12:15:46 -0700

    Fully type transport.py

--- sisyphus_control/transport.py
+++ sisyphus_control/transport.py
@@ -1,16 +1,20 @@
-from typing import Any, Dict, Optional
+from types import TracebackType
+from typing import Any, Awaitable, Callable, Dict, List, Optional, Tuple, Type
 
 import aiohttp
 import asyncio
 import json
 import socketio
 
+TransportCallback = Callable[[
+    Optional[List[Dict[str, Any]]]], Awaitable[None]]
+
 
 class TableTransport:
     def __init__(
             self,
-            ip,
-            callback=None,
+            ip: str,
+            callback: Optional[TransportCallback] = None,
             session: Optional[aiohttp.ClientSession] = None):
         self._session = session
         self._ip = ip
@@ -19,18 +23,18 @@ class TableTransport:
         self._event_loop = asyncio.get_event_loop()
         self._socket_closed = self._event_loop.create_task(self._run_socket())
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> 'TableTransport':
         return self
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, exc_type: Optional[Type[BaseException]], exc_val: Optional[BaseException], exc_tb: Optional[TracebackType]) -> bool:
         await self.close()
         return False
 
     @property
-    def ip(self):
+    def ip(self) -> str:
         return self._ip
 
-    async def close(self):
+    async def close(self) -> None:
         if self._socket_closed:
             self._wants_to_close = True
             await self._socket_closed
@@ -39,7 +43,7 @@ class TableTransport:
             self,
             endpoint: str,
             data: Dict[str, Any] = None,
-            timeout: float = 5):
+            timeout: float = 5) -> List[Dict[str, Any]]:
         return await post(
             self._ip,
             endpoint,
@@ -47,16 +51,18 @@ class TableTransport:
             timeout,
             session=self._session)
 
-    async def _run_socket(self):
+    async def _run_socket(self) -> None:
         sio = socketio.AsyncClient()
 
         @sio.event
-        async def disconnect():
-            await self._callback(None)
+        async def disconnect() -> None:
+            if self._callback is not None:
+                await self._callback(None)
 
         @sio.event
-        async def set(*args):
-            await self._callback(args)
+        async def set(updates: List[Dict[str, Any]]) -> None:
+            if self._callback is not None:
+                await self._callback(updates)
 
         await sio.connect("http://{ip}:{port}".format(ip=self._ip, port=3002))
 
@@ -72,7 +78,7 @@ async def post(
         endpoint: str,
         data: Dict[str, Any] = None,
         timeout: float = 5,
-        session: Optional[aiohttp.ClientSession] = None):
+        session: Optional[aiohttp.ClientSession] = None) -> List[Dict[str, Any]]:
 
     if not session:
         async with aiohttp.ClientSession() as session:
@@ -92,7 +98,7 @@ async def post(
         async with session.post(
                 url,
                 data=form_data,
-                timeout=aiohttp.ClientTimeout(sock_connect=timeout)) as r:
+                timeout=aiohttp.ClientTimeout(sock_connect=timeout)) as r:  # type: ignore
             r = await r.json()
             if r["err"]:
                 raise Exception(r["error"])
